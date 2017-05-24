fb3717042c5defd3cf4979b3cbc4786f140c9d27
artem_chesnokov <artem@chesnokov.pp.ua>
2017-05-24 14:42:08 +0300

    Encrypt and decrypt files on the client side - fix carousel

--- js/manager.js
+++ js/manager.js
@@ -95,6 +95,7 @@ module.exports = function (oAppData) {
 						});
 					});
 					filesCollection(collection);
+					App.broadcastEvent('FileViewerWebclientPlugin::FilesCollection::after', {aFilesCollection: filesCollection});
 				});
 			});
 		},
